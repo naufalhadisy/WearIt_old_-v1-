276cc96a2843321f918ddd61c0780a11da0df7e6
ammarha123 <92913728+ammarha123@users.noreply.github.com>
2021-11-25 14:27:10 +0700

    Add database connection and remove useless stuff

--- php/forum.php
+++ php/forum.php
@@ -1,6 +1,7 @@
 <?php 
 $identifier = 2;
-include("page_header.php"); ?>
+include("page_header.php"); 
+include('dbconnection.php');?>
 
 <section class="s2">
 <div class="container-fluid">
@@ -33,118 +34,53 @@ include("page_header.php"); ?>
 				<div class="card">
 					<div class="card-header">
 						<b>Thread List</b>
-						<span class="">
+						
+				<span class="">
 							<a href="createthread.php" class="btn btn-primary btn-block btn-sm col-sm-2 float-right"><i class="fa fa-plus"></i> <span>Create Thread</span></a>
 					</div>
+					<?php
+                $ret = mysqli_query($con, "select * from topics ");
+                $cnt = 1;
+                $row = mysqli_num_rows($ret);
+                if ($row > 0) {
+                    while ($row = mysqli_fetch_array($ret)) {
+
+                ?>
 					<div class="card-body">
 						<ul class="w-100 list-group" id="topic-list">
 						<li class="list-group-item mb-4">
-							<a href=# style="color:blue;">Sample Post</a>
+						<h2><?php echo $row['title']; ?></h2>
 							<hr>
 							
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-						Donec elementum nunc bibendum, luctus diam id, tincidunt nisl. 
-						Vestibulum turpis arcu, fringilla sed lacus in, eleifend vulputate purus.
+							<a><?php echo $row['content']; ?></a>
+							<img src="img/<?php echo $row['topicFile']; ?>" style="width:100%">
 						 <a style="color: blue"; href="#">Read more...</a>
 					
 					<hr>		
-					<p class="row justify-content-left"><span class="badge badge-success text-white"><i>  Posted By: Naufal Hadi </i></span></p>
-					<span><button type="button" class="btn btn-info"> <a href="editthread.php">Edit</button></span></a>
+					<p class="row justify-content-left"><span class="badge badge-success text-white"><i>  Posted By: <?php echo $row['postedby'];?> </i></span></p>
+					<span><button type="button" class="btn btn-info"> <a href="editthread.php?id=<?php echo $row['id']; ?>">Edit</button></span></a>
 								<hr>
-								
-								<span class="float-left badge badge-secondary text-white"> 10 view/s</span>
-								<span class="float-left badge badge-primary text-white ml-2"><i class="fa fa-comments"></i> 3 comment/s </span>
 									<span class="float-right">
-									<span>Tags: </span>
-									<span class="badge badge-info text-white ml-2">sample</span>
-			
-							
-						
-					
-					</div>
-					<div class="card-body">
-						<ul class="w-100 list-group" id="topic-list">
-						<li class="list-group-item mb-4">
-					
-							<a href=# style="color:blue;">Sample Post 2</a>
-							<hr>
-							
-							Sample TopicSed porta nisi quis nunc gravida, ut ornare velit vulputate.
-							 Aenean dictum mauris suscipit ante imperdiet tincidunt. 
-							 Nulla accumsan mauris eu libero semper, eget faucibus mi vulputate. 
-						 <a style="color: blue"; href="#">Read more...</a>
-					
-					<hr>		
-					<p class="row justify-content-left"><span class="badge badge-success text-white"><i>  Posted By: Ramadhan Muhammad </i></span></p>
-								<hr>
-								
-								<span class="float-left badge badge-secondary text-white"> 10 view/s</span>
-								<span class="float-left badge badge-primary text-white ml-2"><i class="fa fa-comments"></i> 3 comment/s </span>
-									<span class="float-right">
-									<span>Tags: </span>
-									<span class="badge badge-info text-white ml-2">sample</span>
-			
-					</div>
-				</div>
+									<span>Tags:  </span>
+									<span class="badge badge-info text-white ml-2"><?php echo $row['category']; ?></span>
+								</div>
+								<?php
+                        $cnt = $cnt + 1;
+                    }
+                } else { ?>
+                    <tr>
+                        <th style="text-align:center; color:red;" colspan="6">No Record Found</th>
+                    </tr>
+                <?php } ?>
 				</div>
 				
+				</div>
+				
+						
+				
 			</div>
-			<!-- Table Panel -->
 		</div>
 	</div>	
 
 </div>
-<style>
-	
-	td{
-		vertical-align: middle !important;
-	}
-	td p{
-		margin: unset
-	}
-	img{
-		max-width:100px;
-		max-height:150px;
-	}
-</style>
-<script>
-	$(document).ready(function(){
-		$('table').dataTable()
-	})
-	$('#topic-list').JPaging({
-	    pageSize: 15,
-	    visiblePageSize: 10
-
-	  });
-
-	$('#new_topic').click(function(){
-		uni_modal("New Entry","manage_topic.php",'mid-large')
-	})
-	
-	$('.edit_topic').click(function(){
-		uni_modal("Edit Topic","manage_topic.php?id="+$(this).attr('data-id'),'mid-large')
-		
-	})
-	$('.delete_topic').click(function(){
-		_conf("Are you sure to delete this topic?","delete_topic",[$(this).attr('data-id')],'mid-large')
-	})
-
-	function delete_topic($id){
-		start_load()
-		$.ajax({
-			url:'ajax.php?action=delete_topic',
-			method:'POST',
-			data:{id:$id},
-			success:function(resp){
-				if(resp==1){
-					alert_toast("Data successfully deleted",'success')
-					setTimeout(function(){
-						location.reload()
-					},1500)
-
-				}
-			}
-		})
-	}
-</script>
 </section>
