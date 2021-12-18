d37edd36899956d277541d34fcb1af579030b723
Caryn12 <87209562+Caryn12@users.noreply.github.com>
2021-12-18 20:13:38 +0700

    Add files via upload

---
+++ php/howto.php
@@ -0,0 +1,19 @@
+<?php
+$identifier = 1;
+include("page_header.php");
+include("../config.php");
+
+?>
+
+
+<div class="main-container">
+    <section class="s2">
+        <h1>How to</h1>
+
+    </section>
+</div>
+
+</div>
+
+
+<?php include("page_footer.php"); ?> 
