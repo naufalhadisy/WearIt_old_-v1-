f2af13e76ecc317da0dd79c4e20ee88e1ce6f17b
Caryn12 <87209562+Caryn12@users.noreply.github.com>
2021-12-18 20:08:44 +0700

    fixing index

--- php/index.php
+++ php/index.php
@@ -1,62 +1,72 @@
-<?php 
+<?php
 $identifier = 0;
 include("page_header.php"); ?>
 
-            <div class="hero">
+<div class="hero">
+    <div class="main-container">
+        <div class="content-wrapper-1">
+            <div class="hero-word">
                 <h1>WearIt</h1>
                 <h3>Share The Clothes, Connect The Threads</h3>
-                <br>
-                <br>
             </div>
+            <div class="hero-picture">
+                <img src="../img/wearit-logo-removebg.png" width="100%">
+            </div>
+        </div>
+    </div>
+    <br>
+    <br>
 </div>
-    <section class="s2">
-        <div class="main-container">
-            <div class="content-wrapper-1">
-                <div class="hero-word">
-                    <h1>How to Register</h1>
-                    <p>XYZ</p>
-                    <div id="learnmore-button">
-                        <a href="registinfo.php">LEARN MORE</a>
-                    </div>
-                </div>
-                <div class="hero-picture">
-                    <img src="../img/pict1.png">
+</div>
+<div class="s3"></div>
+<section class="s2">
+    <div class="main-container">
+        <div class="content-wrapper-1">
+            <div class="hero-word">
+                <h1>How to Register</h1>
+                <p>XYZ</p>
+                <div id="learnmore-button">
+                    <a href="registinfo.php">LEARN MORE</a>
                 </div>
             </div>
+            <div class="hero-picture">
+                <img src="../img/pict1.png">
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="s3">
-        <div class="main-container">
-            <div class="content-wrapper-2">
-                <div class="hero-word">
-                    <h1>About Us</h1>
-                    <p>XYZ</p>
-                    <div id="learnmore-button">
-                        <a href="aboutus.php">LEARN MORE</a>
-                    </div>
-                </div>
-                <div class="hero-picture">
-                    <img src="../img/pict2.png">
+<section class="s3">
+    <div class="main-container">
+        <div class="content-wrapper-2">
+            <div class="hero-word">
+                <h1>About Us</h1>
+                <p>XYZ</p>
+                <div id="learnmore-button">
+                    <a href="aboutus.php">LEARN MORE</a>
                 </div>
             </div>
+            <div class="hero-picture">
+                <img src="../img/pict2.png">
+            </div>
         </div>
-    </section>
+    </div>
+</section>
 
-    <section class="s2">
-        <div class="main-container">
-            <div class="content-wrapper-1">
-                <div class="hero-word">
-                    <h1>FAQ</h1>
-                    <p>XYZ</p>
-                    <div id="learnmore-button">
-                        <a href="faq.php">LEARN MORE</a>
-                    </div>
-                </div>
-                <div class="hero-picture">
-                    <img src="../img/pict3.png">
+<section class="s2">
+    <div class="main-container">
+        <div class="content-wrapper-1">
+            <div class="hero-word">
+                <h1>FAQ</h1>
+                <p>XYZ</p>
+                <div id="learnmore-button">
+                    <a href="faq.php">LEARN MORE</a>
                 </div>
             </div>
+            <div class="hero-picture">
+                <img src="../img/pict3.png">
+            </div>
         </div>
-    </section>
-    <?php include("page_footer.php"); ?>    
+    </div>
+</section>
+<?php include("page_footer.php"); ?>
